e39907ab895861f67de460af5ce129878f308651
Goitse-Modimo <goitse101makhene18@gmail.com>
2021-10-26 20:48:53 +0200

    initial commit (WIP)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuex from 'vuex'
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
 
+Vue.use(vuex)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
